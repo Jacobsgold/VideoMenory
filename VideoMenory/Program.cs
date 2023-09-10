c94e2c5afbe17cc280ebc348f7e0f431b78458d9
Яков Кудрявцев <kudyakov687@mail.ru>
2023-09-10 23:18:31 +0700

    Добавьте файлы проекта.

---
+++ VideoMenory/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using OpenHardwareMonitor.Hardware;
+
+namespace GPUHealthChecker
+{
+    class Program
+    {
+        static Computer computer;
+
+        static void Main(string[] args)
+        {
+            // Создаем экземпляр компьютера
+            computer = new Computer();
+            // Настройка для получения данных о видеокарте
+            computer.GPUEnabled = true;
+            computer.Open();
+
+            // Получаем первую видеокарту в системе
+            var gpu = computer.Hardware[0];
+
+            // Выводим информацию о видеокарте
+            Console.WriteLine("Модель видеокарты: " + gpu.Name);
+            Console.WriteLine("Температура: " + GetGPUTemperature(gpu) + "°C");
+            Console.WriteLine("Скорость вентилятора: " + GetGPUFanSpeed(gpu) + " RPM");
+
+            // Закрываем соединение с компьютером
+            computer.Close();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода.");
+            Console.ReadKey();
+        }
+
+        static float GetGPUTemperature(IHardware gpu)
+        {
+            gpu.Update();
+            foreach (var sensor in gpu.Sensors)
+            {
+                if (sensor.SensorType == SensorType.Temperature && sensor.Name == "GPU Core")
+                {
+                    return sensor.Value ?? 0;
+                }
+            }
+            return 0;
+        }
+
+        static int GetGPUFanSpeed(IHardware gpu)
+        {
+            gpu.Update();
+            foreach (var sensor in gpu.Sensors)
+            {
+                if (sensor.SensorType == SensorType.Fan && sensor.Name == "GPU Fan")
+                {
+                    return (int)(sensor.Value ?? 0);
+                }
+            }
+            return 0;
+        }
+    }
+}
